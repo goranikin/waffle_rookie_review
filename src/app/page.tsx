444bd93c4b53a77dbaae8f71f7773e0763f69499
goranikin <goranikin@snu.ac.kr>
2025-03-01 12:23:52 +0900

    자동 배포 테스트 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,9 @@ export default function Home() {
   return (
     <div>
       <p>랜딩페이지 입니다.</p>
-      <p>테스트 메시지!!!!</p>
+      <p>테스트 메시지1</p>
+      <p>테스트 메시지2</p>
+      <p>테스트 메시지3</p>
       <button type="button" onClick={() => router.push('/main')}>
         메인페이지로 이동
       </button>
