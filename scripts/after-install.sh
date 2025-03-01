5f97f6fab38ba8e86d05d3bb439f12b70676d30f
goranikin <goranikin@snu.ac.kr>
2025-03-01 11:55:27 +0900

    자동 배포 테스트 1

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,1 +1,14 @@
-echo "> after install"
+#!/bin/bash
+
+echo "> After install script started"
+
+# Navigate to the project directory
+cd /home/ubuntu/waffle_rookie_review || exit
+
+# Install dependencies (if necessary)
+bun install
+
+# Restart the PM2 process
+pm2 restart next-app
+
+echo "> After install script completed"
