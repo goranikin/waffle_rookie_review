d1d9687ed08d75262a1c0e5965df508e881243e4
goranikin <goranikin@snu.ac.kr>
2025-03-03 17:41:51 +0900

    제발 되어라~~

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
   return (
     <div>
       <p>랜딩페이지 입니다.</p>
-      <p>테스트</p>
+      <p>진짜 테스트!</p>
       <button type="button" onClick={() => router.push('/main')}>
         메인페이지로 이동
       </button>
