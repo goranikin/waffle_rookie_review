cd68cbcac9523f3270eaf45be8a69f9b56e7f405
goranikin <goranikin@snu.ac.kr>
2025-03-05 12:49:03 +0900

    build 실패 hotfix

--- src/app/(routes)/blog/[slug]/page.tsx
+++ src/app/(routes)/blog/[slug]/page.tsx
@@ -2,14 +2,14 @@ import { getPostBySlug, getPostList } from '@/util/post';
 import { notFound } from 'next/navigation';
 
 type Props = {
-  params: {
+  params: Promise<{
     slug: string;
-  };
+  }>;
 };
 
 export default async function PostPage({ params }: Props) {
   const postList = await getPostList();
-  const slug = params.slug;
+  const slug = (await params).slug;
   const post = getPostBySlug({ slug, postList });
 
   if (!post) {
